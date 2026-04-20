65b256c224feb6e4ad8dcb61de3d81e969680add
azawat7 <az4wat@gmail.com>
2026-04-20 21:22:46 +0200

    fix(hypr): toggle fuzzel on Super key

--- hypr/scripts/mic-toggle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pactl set-source-mute @DEFAULT_SOURCE@ toggle
-STATE=$(pactl get-source-mute @DEFAULT_SOURCE@ | awk '{print $2}')
-if [ "$STATE" = "yes" ]; then
-	notify-send -i microphone-sensitivity-muted "Mic Muted"
-else 
-	notify-send -i microphone-sensitivity-high "Mic Active"
-fi
