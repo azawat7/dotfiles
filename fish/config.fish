70d0d950656a6912ce327b3933b524d3ba1d2c6a
azawat7 <az4wat@gmail.com>
2026-04-21 21:41:21 +0200

    chore(fish): remove fisher and nvm, add dotnet to PATH

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,7 @@ if status is-interactive
 # Commands to run in interactive sessions can go here
 end
 starship init fish | source
-export PATH="$HOME/.local/bin:$PATH"
+
+export DOTNET_ROOT=$HOME/.dotnet
+export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
+set -gx PATH $HOME/.local/bin $PATH
