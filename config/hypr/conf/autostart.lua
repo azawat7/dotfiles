3c077dda84f238076fedef722f07fc75121fa557
azawat7 <az4wat@gmail.com>
2026-08-11 18:08:21 +0200

    feat(pkgs): add and setup awww

--- config/hypr/conf/autostart.lua
+++ config/hypr/conf/autostart.lua
@@ -1,5 +1,6 @@
 hl.on("hyprland.start", function () 
    hl.exec_cmd("systemctl --user start hyprpolkitagent")
+   hl.exec_cmd("systemctl --user start awww-daemon")
 end)
 
 hl.env("XCURSOR_SIZE", "24")
