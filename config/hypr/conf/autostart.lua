2d4d4f03a92687f8bf5d6b5d285fe6cf726051a8
azawat7 <az4wat@gmail.com>
2026-08-12 01:08:15 +0200

    fix(hypr): launch awww-daemon directly

--- config/hypr/conf/autostart.lua
+++ config/hypr/conf/autostart.lua
@@ -1,6 +1,6 @@
 hl.on("hyprland.start", function () 
    hl.exec_cmd("systemctl --user start hyprpolkitagent")
-   hl.exec_cmd("systemctl --user start awww-daemon")
+   hl.exec_cmd("awww-daemon")
 end)
 
 hl.env("XCURSOR_SIZE", "24")
