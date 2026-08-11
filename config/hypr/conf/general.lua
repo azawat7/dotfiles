19d39c35405ff308eb4595e22e7ac4658e0d3371
azawat7 <az4wat@gmail.com>
2026-08-11 18:34:21 +0200

    style(hypr): slim borders and rounding

--- config/hypr/conf/general.lua
+++ config/hypr/conf/general.lua
@@ -11,7 +11,7 @@ hl.config({
         gaps_in  = 5,
         gaps_out = 20,
 
-        border_size = 2,
+        border_size = 1,
 
         col = {
             active_border   = colors.active_border or "rgba(33ccffee)",
@@ -26,7 +26,7 @@ hl.config({
     },
 
     decoration = {
-        rounding       = 10,
+        rounding       = 8,
         rounding_power = 2,
 
         active_opacity   = 1.0,
